4fc360129abb6ee76254c7e551a86f841e993a9b
Andrew McDonough <andrew@andrewmcdonough.co.uk>
2016-03-28 11:25:18 +0100

    When an email in the inbox loads, replace the from address
    Replaced with a link to search all other email in the inbox from that
    address.

--- inboxnil.js
+++ inboxnil.js
@@ -1,6 +1,20 @@
-var email = document.getElementsByClassName("go")[0].innerHTML;
-var escapedEmail = escape(email);
-var newLocation = "https://mail.google.com/mail/u/0/#search/from%3A"+escapedEmail+"+in%3Ainbox";
-var senderName = document.getElementsByClassName("gD")[0].innerHTML;
-var newLink = "<a href='"+newLocation+"'>senderName</a>";
-document.getElementsByClassName("gD")[0].innerHTML = newLink;
+function checkHash() {
+    if (/^#(inbox)\b/.test(location.hash)) {
+			replaceSender();
+    }
+}
+window.addEventListener('hashchange', checkHash);
+checkHash();
+
+function replaceSender() {
+	var goElements = document.getElementsByClassName("go");
+
+	if (goElements.length > 0) {
+		var email = goElements[0].textContent;
+		var escapedEmail = escape(email);
+		var newLocation = "https://mail.google.com/mail/u/0/#search/from%3A"+escapedEmail+"+in%3Ainbox";
+		var senderName = document.getElementsByClassName("gD")[0].innerHTML;
+		var newLink = "<a href='"+newLocation+"'>"+senderName+"</a>";
+		document.getElementsByClassName("gD")[0].innerHTML = newLink;
+	}
+}
